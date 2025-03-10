8274cd50b3539a2c3cb2209bad9dfe052c229e0f
Evan <spanglem@uc.edu>
2025-03-09 20:37:42 -0400

    Final Commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {Scanner scanner = new Scanner(System.in);
+        boolean playAgain = true;
+        while (playAgain) {
+            int die1 = rollDie();
+            int die2 = rollDie();
+            int sum = die1 + die2;
+            System.out.println("You rolled: " + die1 + " + " + die2 + " = " + sum);
+            if (sum == 2 || sum == 3 || sum == 12) {
+                System.out.println("Craps! You lose.");
+            } else if (sum == 7 || sum == 11) {
+                System.out.println("Natural! You win.");
+            } else {
+                int point = sum;
+                System.out.println("Your point is " + point);
+                boolean tryingForPoint = true;
+                while (tryingForPoint) {
+                    System.out.println("Rolling for the point...");
+                    die1 = rollDie();
+                    die2 = rollDie();
+                    sum = die1 + die2;
+
+                    System.out.println("You rolled: " + die1 + " + " + die2 + " = " + sum);
+
+                    if (sum == point) {
+                        System.out.println("You made the point! You win.");
+                        tryingForPoint = false;
+                    } else if (sum == 7) {
+                        System.out.println("You rolled a 7! You lose.");
+                        tryingForPoint = false;
+                    }
+                }
+            }
+            System.out.print("Do you want to play again? (yes/no): ");
+            String userInput = scanner.nextLine().toLowerCase();
+            if (!userInput.equals("yes")) {
+                playAgain = false;
+            }
+        }
+
+        System.out.println("Thanks for playing!");
+    }
+    public static int rollDie() {
+        Random rnd = new Random();
+        return rnd.nextInt(6) + 1;
+    }
+}
